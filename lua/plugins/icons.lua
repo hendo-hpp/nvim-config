1ea441c2133173542a6530dd4fe11202594ded72
hendo-hpp <270048391+hendo-hpp@users.noreply.github.com>
2026-05-30 15:35:58 -0400

    Update cmake and makefile icons

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -31,6 +31,7 @@ local target_file_names = {
     ['.gitignore'] = true,
     ['.gitattributes'] = true,
     ['Makefile'] = true,
+    ['CMakeLists.txt'] = true,
 }
 
 
@@ -40,14 +41,23 @@ local file_overrides = {}
 
 
 local purple = '#B392F0'
+local gear_icon = '󰒓'
 
 
 for ext, default_data in pairs(devicons.get_icons_by_extension()) do
     if target_file_extensions[ext] then
+        local icon = default_data.icon
+        local name = default_data.name
+
+        if ext == 'cmake' then
+            icon = gear_icon
+            name = 'CMake'
+        end
+
         ext_overrides[ext] = {
-            icon = default_data.icon,
+            icon = icon,
             color = purple,
-            name = default_data.name
+            name = name
         }
     end
 end
@@ -55,10 +65,23 @@ end
 
 for fname, default_data in pairs(devicons.get_icons_by_filename()) do
     if target_file_names[fname] then
+        icon = default_data.icon
+        name = default_data.name
+
+        if fname == 'Makefile' then
+            icon = gear_icon
+            name = 'Makefile'
+        end
+
+        if fname == 'CMakeLists.txt' then
+            icon = gear_icon
+            name = 'CMakeLists.txt'
+        end
+
         file_overrides[fname] = {
-            icon = default_data.icon,
+            icon = icon,
             color = purple,
-            name = default_data.name
+            name = name
         }
     end
 end
@@ -67,5 +90,16 @@ end
 devicons.setup({
     default = true,
     override = ext_overrides,
-    override_by_filename = file_overrides,
+    override_by_filename = vim.tbl_extend('force', file_overrides, {
+        ['CMakeLists.txt'] = {
+            icon = gear_icon,
+            color = purple,
+            name = 'CMakeLists.txt'
+        },
+        ['Makefile'] = {
+            icon = gear_icon,
+            color = purple,
+            name = 'CMakeLists.txt'
+        },
+    })
 })
